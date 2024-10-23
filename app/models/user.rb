15c1f28a0715d343b8c67a7926d86d1bd8d8e29f
Lanzema Leche <lleche@DESKTOP-QQ49ALV>
2024-10-23 11:07:29 -0500

    Add user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :transactions
   has_and_belongs_to_many :cryptocurrencies
 
+  # Validations for each
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
