329a73b7204f1905810cff7bcefd36d7bc5790de
Lanzema Leche <lleche@DESKTOP-QQ49ALV>
2024-10-23 11:06:48 -0500

    Add transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,7 +2,7 @@ class Transaction < ApplicationRecord
   belongs_to :user
   belongs_to :cryptocurrency
 
-  # Validations
+  # Validations for each
   validates :amount, numericality: { greater_than: 0 }
   validates :transaction_type, presence: true, inclusion: { in: ['buy', 'sell'] }
 end
