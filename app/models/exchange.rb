9b38b44de12ee9fb5ee7bb91013ba2c6c6839ad6
Lanzema Leche <lleche@DESKTOP-QQ49ALV>
2024-10-23 11:05:50 -0500

    Add exchange model

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -1,7 +1,7 @@
 class Exchange < ApplicationRecord
   has_and_belongs_to_many :cryptocurrencies
 
-  # Validations
+  # Validations for each
   validates :name, presence: true
   validates :location, presence: true
   validates :latitude, :longitude, numericality: true, allow_nil: true
